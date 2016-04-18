9ed341c1ca2f39bae733921b0b264772232a9067
manoj <manojg@wwso2.com>
2016-04-18 20:55:36 +0530

    DB Client changes

--- scripts/create_mysql_dbs.sh
+++ scripts/create_mysql_dbs.sh
@@ -11,20 +11,24 @@ done
 
 MYSQL_CMD="mysql -h$MYSQL_PORT_3306_TCP_ADDR -P$MYSQL_PORT_3306_TCP_PORT -uroot -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD"
 
+
 $MYSQL_CMD '-e create database dbApimConfig'
 $MYSQL_CMD '-e create database dbUserstore'
 $MYSQL_CMD '-e create database dbGovernance'
 $MYSQL_CMD '-e create database dbApiMgt'
 $MYSQL_CMD '-e create database dbApiStatus'
+$MYSQL_CMD '-e create database dbISConfig'
 
 $MYSQL_CMD '-e grant all on dbApimConfig.* TO ConfigDBUser@"%" identified by "ConfigDBUserPass"'
 $MYSQL_CMD '-e grant all on dbUserstore.* TO UserstoreUser@"%" identified by "UserstoreUserPass"'
 $MYSQL_CMD '-e grant all on dbGovernance.* TO GovernanceUser@"%" identified by "GovernanceUserPass"'
 $MYSQL_CMD '-e grant all on dbApiMgt.* TO ApiMgtUser@"%" identified by "ApiMgtUserPass"'
 $MYSQL_CMD '-e grant all on dbApiStatus.* TO ApiStatuUser@"%" identified by "ApiStatuUserPass"'
+$MYSQL_CMD '-e grant all on dbISConfig.* TO ConfigDBUser@"%" identified by "ConfigDBUserPass"'
 
-$MYSQL_CMD dbApimConfig < /scripts/mysql.sql
-$MYSQL_CMD dbUserstore < /scripts/mysql.sql
-$MYSQL_CMD dbGovernance < /scripts/mysql.sql
+$MYSQL_CMD dbApimConfig < /scripts/apimgt.sql
+$MYSQL_CMD dbUserstore < /scripts/userstore.sql
+$MYSQL_CMD dbGovernance < /scripts/registry.sql
 $MYSQL_CMD dbApiStatus < /scripts/mysql.sql
 $MYSQL_CMD dbApiMgt < /scripts/apimgt.sql
+$MYSQL_CMD dbApiMgt < /scripts/identity.sql
